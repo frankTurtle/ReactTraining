1056a9e6ab97f29bbf19351840dd2930bdc60abd
Barret J. Nobel <bear.nobel@gmail.com>
2018-01-02 14:06:03 -0500

    updated a bit so that the stars so not re-render everytime something is redrawn

--- Game.js
+++ Game.js
@@ -1,82 +1,93 @@
 const Stars = (props) => {
-    const numberOfStars = 1 + Math.floor(Math.random() * 9 );
-    
     return (
-      <div className="col-5">
-          {_.range(numberOfStars).map( i =>
-            <i key={i} className="fa fa-star"></i>
-        )}
-      </div>
-    );
-  };
-  
-  const Button = (props) => {
-    return (
-      <div className="col-2">
-        <button>=</button>
-      </div>
-    );
-  };
-  
-  const Answer = (props) => {
-    return (
-      <div className="col-5">
-        {props.selectedNumbers.map((number, i) =>
-            <span key={i}>{number}</span>
-        )}
-      </div>
-    );
-  };
-  
-  const Numbers = (props) => {
-      const numberClassName = (number) => {
-        if(props.selectedNumbers.indexOf(number) >= 0){
-          return 'selected';
-      }
-    }
-    
-    return (
-      <div className="card text-center">
-        <div>
-          {Numbers.list.map((number, i) =>
-              <span key={i} className={numberClassName(number)}>{number}</span>
-          )}
+        <div className="col-5">
+            {_.range(props.randomNumOfStars).map(i =>
+                <i key={i} className="fa fa-star"></i>
+            )}
         </div>
-      </div>
     );
-  };
-  Numbers.list = _.range(1, 10);
-  
-  class Game extends React.Component {
-      state = {
+};
+
+const Button = (props) => {
+    return (
+        <div className="col-2">
+            <button>=</button>
+        </div>
+    );
+};
+
+const Answer = (props) => {
+    return (
+        <div className="col-5">
+            {props.selectedNumbers.map((number, i) =>
+                <span key={i}>{number}</span>
+            )}
+        </div>
+    );
+};
+
+const Numbers = (props) => {
+    const numberClassName = (number) => {
+        if (props.selectedNumbers.indexOf(number) >= 0) {
+            return 'selected';
+        }
+    }
+
+    return (
+        <div className="card text-center">
+            <div>
+                {Numbers.list.map((number, i) =>
+                    <span
+                        key={i}
+                        className={numberClassName(number)}
+                        onClick={() => props.selectNumber(number)}>{number}</span>
+                )}
+            </div>
+        </div>
+    );
+};
+Numbers.list = _.range(1, 10);
+
+class Game extends React.Component {
+    state = {
         selectedNumbers: [],
+        randomNumOfStars: 1 + Math.floor(Math.random() * 9),
     };
-    
+
+    selectNumber = (clickedNumber) => {
+        this.setState(prevState => ({
+            selectedNumbers: prevState.selectedNumbers.concat(clickedNumber)
+        }));
+    };
+
     render() {
-      return (
-        <div className="container">
-          <h3>Play Nine</h3>
-          <hr />
-          <div className="row">
-            <Stars />
-            <Button />
-            <Answer selectedNumbers={this.state.selectedNumbers} />
-          </div>
-          <br />
-          <Numbers selectedNumbers={this.state.selectedNumbers} />
-        </div>
-      );
+        return (
+            <div className="container">
+                <h3>Play Nine</h3>
+                <hr />
+                <div className="row">
+                    <Stars randomNumOfStars={this.state.randomNumOfStars} />
+                    <Button />
+                    <Answer selectedNumbers={this.state.selectedNumbers} />
+                </div>
+                <br />
+                <Numbers
+                    selectedNumbers={this.state.selectedNumbers}
+                    selectNumber={this.selectNumber}
+                />
+            </div>
+        );
     }
-  }
-  
-  class App extends React.Component {
+}
+
+class App extends React.Component {
     render() {
-      return (
-        <div>
-          <Game />
-        </div>
-      );
+        return (
+            <div>
+                <Game />
+            </div>
+        );
     }
-  }
-  
-  ReactDOM.render(<App />, mountNode);
+}
+
+ReactDOM.render(<App />, mountNode);
