4a9f9a603ab6477a3f3d449d6300bbf177d7b715
Barret J. Nobel <bear.nobel@gmail.com>
2018-01-02 12:27:24 -0500

    updated some implementation of dynamic number / star generation

--- Game.js
+++ Game.js
@@ -1,68 +1,71 @@
 const Stars = (props) => {
+    const numberOfStars = 1 + Math.floor(Math.random() * 9 );
+    
     return (
-        <div className="col-5">
-            <i className="fa fa-star"></i>
-            <i className="fa fa-star"></i>
-            <i className="fa fa-star"></i>
-            <i className="fa fa-star"></i>
-        </div>
-    );
-};
-
-const Button = (props) => {
-    return (
-        <div className="col-2">
-            <button>=</button>
-        </div>
-    );
-};
-
-const Answer = (props) => {
-    return (
-        <div className="col-5">
-            ...
+      <div className="col-5">
+          {_.range(numberOfStars).map( i =>
+            <i key={i} className="fa fa-star"></i>
+        )}
       </div>
     );
-};
-
-const Numbers = (props) => {
+  };
+  
+  const Button = (props) => {
     return (
-        <div className="card text-center">
-            <div>
-                <span>1</span>
-                <span className="selected">2</span>
-                <span className="used">3</span>
-            </div>
-        </div>
+      <div className="col-2">
+        <button>=</button>
+      </div>
     );
-};
-
-class Game extends React.Component {
+  };
+  
+  const Answer = (props) => {
+    return (
+      <div className="col-5">
+        ...
+      </div>
+    );
+  };
+  
+  const Numbers = (props) => {
+    return (
+      <div className="card text-center">
+        <div>
+          {Numbers.list.map((number, i) =>
+              <span key={i}>{number}</span>
+          )}
+        </div>
+      </div>
+    );
+  };
+  
+  Numbers.list = _.range(1, 10);
+  
+  class Game extends React.Component {
     render() {
-        return (
-            <div className="container">
-                <h3>Play Nine</h3>
-                <hr />
-                <div className="row">
-                    <Stars />
-                    <Button />
-                    <Answer />
-                </div>
-                <br />
-                <Numbers />
-            </div>
-        );
+      return (
+        <div className="container">
+          <h3>Play Nine</h3>
+          <hr />
+          <div className="row">
+            <Stars />
+            <Button />
+            <Answer />
+          </div>
+          <br />
+          <Numbers />
+        </div>
+      );
     }
-}
-
-class App extends React.Component {
+  }
+  
+  class App extends React.Component {
     render() {
-        return (
-            <div>
-                <Game />
-            </div>
-        );
+      return (
+        <div>
+          <Game />
+        </div>
+      );
     }
-}
-
-ReactDOM.render(<App />, mountNode);
+  }
+  
+  ReactDOM.render(<App />, mountNode);
