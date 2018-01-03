a066f688d3f5f7e9f68ccece677f6fcae4fbcecf
Barret J. Nobel <bear.nobel@gmail.com>
2018-01-03 16:37:07 -0500

    added state updated person's name based on state

--- udemy-course/src/App.js
+++ udemy-course/src/App.js
@@ -3,11 +3,22 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: 'toby' },
+      { name: 'frank' },
+      { name: 'isaac' },
+    ]
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a react app! WOO</h1>
-        <Person name='isaac'>Hobbies: racing!</Person>
+        <button>Name Switch</button>
+        <Person name={this.state.persons[0].name} />
+        <Person name={this.state.persons[1].name} />
+        <Person name={this.state.persons[2].name}>Hobbies: racing!</Person>
       </div>
     );
   }
