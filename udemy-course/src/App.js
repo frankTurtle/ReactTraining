5d20b7463e78e20862de1f7ca259977b92508b45
Barret J. Nobel <bear.nobel@gmail.com>
2018-01-09 12:09:24 -0500

    mapped array to render JSX object

--- udemy-course/src/App.js
+++ udemy-course/src/App.js
@@ -52,17 +52,12 @@ class App extends Component {
     if (this.state.showPersons) {
       persons = (
         <div>
-          <Person
-            name={this.state.persons[0].name}
-            click={this.swithNameHandler}
-          />
-
-          <Person
-            name={this.state.persons[1].name}
-            changed={this.nameChangedHandler}
-          />
-
-          <Person name={this.state.persons[2].name}>Hobbies: racing!</Person>
+          {this.state.persons.map(person => {
+            return <Person
+              name={person.name}
+              click={this.swithNameHandler}
+            />
+          })}
         </div>
       );
     }
