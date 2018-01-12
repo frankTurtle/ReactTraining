3dfd51052e4e56e04c846880623c0ae92bc13104
Barret J. Nobel <bear.nobel@gmail.com>
2018-01-12 11:40:20 -0500

    added Radium package to add pseudo selector styles - tested with button hover and background color

--- udemy-course/src/App.js
+++ udemy-course/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
+import Radium from 'radium';
 
 class App extends Component {
   state = {
@@ -42,7 +43,10 @@ class App extends Component {
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
-      cursor: 'pointer'
+      cursor: 'pointer',
+      ':hover': {
+        backgroundColor: 'lightgreen'
+      }
     }
 
     let persons = null;
@@ -61,6 +65,10 @@ class App extends Component {
       );
 
       style.backgroundColor = 'red';
+      style[':hover'] = {
+        backgroundColor: 'salmon',
+        color: 'black'
+      }
     }
 
     let classes = [];
@@ -88,4 +96,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default Radium(App);
